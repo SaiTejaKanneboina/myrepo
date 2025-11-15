4b97206b0ffe570704d2d8cfed037f87c7cba90b
SaiTejaKanneboina <saiteja631999@gmail.com>
2025-11-15 16:17:32 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+
+        // Demonstrate a simple for loop
+        System.out.println("Counting from 0 to 4:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Current count: " + i);
+        }
+    }
+}
